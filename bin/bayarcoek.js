0e4c8a9afcd5d0f1a01e6e109f94f93e1ff09951
tfkhdyt <ta142fa@gmail.com>
2021-11-10 20:32:48 +0700

    prettier

--- bin/bayarcoek.js
+++ bin/bayarcoek.js
@@ -10,40 +10,50 @@ const decrypt = require('../lib/utils/decrypt');
 
 // program information
 program
-  .name("bayarcoek")
-  .usage("<command> [options]")
+  .name('bayarcoek')
+  .usage('<command> [options]')
   .version(info.version, '-v, --version', 'Menampilkan versi')
   .showSuggestionAfterError()
   .helpOption('-h, --help', 'Menampilkan bantuan');
-  
+
 // command
 // encrypt
 program
   .command('encrypt [path...]')
   .description('Mengenkripsi file atau folder')
-  .option('-x, --extension <ext>', 'Pilih custom extension untuk hasil enkripsi', 'bayarcoek')
-  .option('-k, --secret-key <key>', 'Secret key untuk enkripsi dan dekripsi', 'N7wKWb5434FLD')
+  .option(
+    '-x, --extension <ext>',
+    'Pilih custom extension untuk hasil enkripsi',
+    'bayarcoek'
+  )
+  .option(
+    '-k, --secret-key <key>',
+    'Secret key untuk enkripsi dan dekripsi',
+    'N7wKWb5434FLD'
+  )
   .action((path, options) => {
     // encrypt(path);
     if (path.length == 0) path.push('');
     path.forEach((p) => {
-      const _path = process.cwd() + '/' + p;
       encrypt(p, options.extension, options.secretKey);
     });
   });
-  
+
 // decrypt
 program
   .command('decrypt [path...]')
   .description('Mendekripsi file atau folder')
-  .option('-k, --secret-key <key>', 'Secret key untuk enkripsi dan dekripsi', 'N7wKWb5434FLD')
+  .option(
+    '-k, --secret-key <key>',
+    'Secret key untuk enkripsi dan dekripsi',
+    'N7wKWb5434FLD'
+  )
   .action((path, options) => {
     if (path.length == 0) path.push('');
     path.forEach((p) => {
-      const _path = process.cwd() + '/' + p;
       decrypt(p, options.secretKey);
     });
   });
-  
+
 // option
 program.parse();
