0ca5d2c78c166e3ceb84419974afdc5c9c9f0f1f
tfkhdyt <ta142fa@gmail.com>
2021-11-10 11:40:58 +0700

    ganti urutan index argv

--- bayarcoek.js
+++ bayarcoek.js
@@ -9,12 +9,12 @@ key = crypto
   .digest('base64')
   .substr(0, 32);
 
-const mode = process.argv[2];
+const mode = process.argv[1];
 if (!['encrypt', 'decrypt'].includes(mode)) {
   console.log(`${mode} apaan bro? Gk paham sy`);
   process.exit(1);
 }
-const extension = process.argv[3] || process.env.BAYARCOEK_EXT || 'bayarcoek';
+const extension = process.argv[2] || process.env.BAYARCOEK_EXT || 'bayarcoek';
 let count = 0;
 const _path = __dirname + '/';
 
