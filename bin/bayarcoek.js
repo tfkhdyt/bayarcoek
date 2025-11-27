45247c4c0758b5edfb5f5a0389db9527731938c5
Taufik Hidayat <taufik@generativestudio.dev>
2025-11-27 09:41:40 +0700

    fix: make bin script executable and format code

--- bin/bayarcoek.js
+++ bin/bayarcoek.js
@@ -1,59 +1,59 @@
 #!/usr/bin/env node
 // modules
-const { Command } = require('commander')
-const program = new Command()
+const { Command } = require("commander");
+const program = new Command();
 
 // imported files
-const info = require('../package.json')
-const encrypt = require('../lib/utils/encrypt')
-const decrypt = require('../lib/utils/decrypt')
+const info = require("../package.json");
+const encrypt = require("../lib/utils/encrypt");
+const decrypt = require("../lib/utils/decrypt");
 
 // program information
 program
-  .name('bayarcoek')
-  .usage('<command> [options]')
-  .version(info.version, '-v, --version', 'Menampilkan versi')
+  .name("bayarcoek")
+  .usage("<command> [options]")
+  .version(info.version, "-v, --version", "Menampilkan versi")
   .showSuggestionAfterError()
-  .helpOption('-h, --help', 'Menampilkan bantuan')
+  .helpOption("-h, --help", "Menampilkan bantuan");
 
 // command
 // encrypt
 program
-  .command('encrypt [path...]')
-  .description('Mengenkripsi file atau folder')
+  .command("encrypt [path...]")
+  .description("Mengenkripsi file atau folder")
   .option(
-    '-x, --extension <ext>',
-    'Pilih custom extension untuk hasil enkripsi',
-    'bayarcoek'
+    "-x, --extension <ext>",
+    "Pilih custom extension untuk hasil enkripsi",
+    "bayarcoek"
   )
   .option(
-    '-k, --secret-key <key>',
-    'Secret key untuk enkripsi dan dekripsi',
-    'N7wKWb5434FLD'
+    "-k, --secret-key <key>",
+    "Secret key untuk enkripsi dan dekripsi",
+    "N7wKWb5434FLD"
   )
   .action((path, options) => {
-    if (path.length === 0) path.push('')
+    if (path.length === 0) path.push("");
     path.forEach((p) => {
-      encrypt(p, options.extension, options.secretKey)
-    })
-  })
+      encrypt(p, options.extension, options.secretKey);
+    });
+  });
 
 // decrypt
 program
-  .command('decrypt [path...]')
-  .description('Mendekripsi file atau folder')
+  .command("decrypt [path...]")
+  .description("Mendekripsi file atau folder")
   .option(
-    '-k, --secret-key <key>',
-    'Secret key untuk enkripsi dan dekripsi',
-    'N7wKWb5434FLD'
+    "-k, --secret-key <key>",
+    "Secret key untuk enkripsi dan dekripsi",
+    "N7wKWb5434FLD"
   )
-  .option('-o, --overwrite', 'Timpa file hasil decrypt')
+  .option("-o, --overwrite", "Timpa file hasil decrypt")
   .action((path, options) => {
-    if (path.length === 0) path.push('')
+    if (path.length === 0) path.push("");
     path.forEach((p) => {
-      decrypt(p, options.secretKey, options.overwrite)
-    })
-  })
+      decrypt(p, options.secretKey, options.overwrite);
+    });
+  });
 
 // option
-program.parse()
+program.parse();
